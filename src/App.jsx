689cda4b89b1b935b0bfedef8ee23a98c63d7e9f
manish <manish26eleven@gmail.com>
2025-11-21 20:29:07 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -22,6 +22,7 @@ function App() {
      <Header/>
      
       <Routes>
+      
             <Route path="/" element={<Home />} />
              <Route path="/ground-owners" element={<GroundOwners />} />
               <Route path="/players" element={<Players />} />
